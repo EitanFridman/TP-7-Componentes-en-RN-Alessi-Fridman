c0ece82d0df379bf97b8485e85eaf5afdf88a28f
Android Studio <devandroid@ort>
2025-05-12 10:42:35 -0300

    tp casi terminado

--- TP7-componentes-en-RN/App.js
+++ TP7-componentes-en-RN/App.js
@@ -1,25 +1,62 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, ImageBackground, TextComponent } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground, Image, Button, Alert, SafeAreaView, ScrollView, TextInput } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
-    
-      <ImageBackground
-       source={{ uri: 'https://i.imgur.com/5EOyTDQ.jpg' }}
-      style = {styles.backgroundImage}
-      resizemode = "cover">
-      <Text style={styles.text}>Mi</Text>
-      </ImageBackground>
 
-      <View style = {styles.card}>
-      <Image 
-      source ={require('./assets/images/profile.jpg')}
-      style = {styles.image}/>
-      </View>
-      <Text style = {styles.nombre}> Rabin Iztak</Text>
-      <Text
+    <View style={styles.container}>
+      <StatusBar style="light" />
+      
+
+      <ImageBackground
+        source={require('./assets/background.jpg')}
+        style={styles.backgroundImage}
+        resizeMode="cover">
+          
+        <ScrollView>
+        <SafeAreaView style={{flex: 100}}>
+        <Text style={styles.text}>Mi Carta Personal</Text>
+            {/* <View style={styles.card}>
+              <Image 
+                source={require('./assets/profile.jpg')}
+                style={styles.image}
+              />
+              
+              <Text style={styles.nombre}>Rabin Iztak</Text>
+              <Text style={styles.ocupacion}>Programador</Text>
+              <Text style={styles.descripcion}>Hola, soy programador de react native y mi materia favorita es DAI</Text>
+
+              <Button 
+              styles={styles.button}
+              title= 'Ver Perfil'
+              onPress = {()=> Alert.alert("Hola")}/>
+            </View> */}
+
+            <View style={styles.card}>
+              <Image 
+                source={require('./assets/profile.jpg')}
+                style={styles.image}
+              />
+              
+              <Text style={styles.nombre}>Rabin Iztak</Text>
+              <Text style={styles.ocupacion}>Programador</Text>
+
+              <TextInput style= {styles.input} 
+              placeholder='Escriba su comentario'>
+              </TextInput>
+
+              <View style = {styles.fondoB}>
+                <Button 
+                styles ={styles.button}
+                title = 'Ver Perfil'
+                onPress = {()=> Alert.alert("Hola")}/>
+              </View>
+           
+            </View>
+
+        </SafeAreaView>
+        </ScrollView>
+      </ImageBackground>
     </View>
   );
 }
@@ -27,16 +64,58 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
   backgroundImage: {
     flex: 1,
     width: '100%',
     height: '100%',
+    alignItems: 'center',
     justifyContent: 'center',
-    alignItems: 'center',   
   },
+  text: {
+    fontSize: 35,
+    color: 'white',
+    marginBottom: 40,
+    fontWeight: 'bold', 
+  },
+  card: {
+    backgroundColor: 'rgba(255,255,255,0.9)',
+    padding: 30,
+    borderRadius: 25,
+    alignItems: 'center',
+    width: 260,
+    margin: 30,
+  
+  },
+  image: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    marginBottom: 20,
+  },
+  nombre: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    
+  },
+  ocupacion: {
+    fontSize: 16,
+    color: '#555',
 
+  },
+  descripcion: {
+    fontSize: 14,
+    textAlign: 'center',
+    marginTop: 10,
+    marginBottom: 20,
+    color: '#333',
+  },
+  button:
+  {
+    
+  },
+  fondoB:{
+    backgroundColor: 'blue',
+    borderRadius: 10,
+  }
 });
